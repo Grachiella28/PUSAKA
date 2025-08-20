7a08cf6064898ccf7d8e08e8b25be717556f8344
grachiella28 <grachielladian@gmail.com>
2025-08-20 22:18:53 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../styles/navbar.css";
+import "../styles/Navbar.css";
 import logo from "../assets/Logo.png";
 
 const Navbar = React.forwardRef((props, ref) => {
